01f0a76c5505e528e88b44a6f7c8c7d81a086284
abyoo <helios@tests-MBP.hitronhub.home>
2018-03-10 20:57:30 -0800

    initial attempt, not working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
 import './App.css';
+<<<<<<< 04c3eeb13ee6c35ac74cb5f8d1155fc8bc5740d4
 import CourseSelectionLayout from './components/course-selection-layout';
 import CourseInformationForm from './components/course-information-form';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+=======
+import CourseList from './components/course-list';
+import YearList from './components/course-selection-page';
+import SignUp from './components/SignUp'
+import { Year, Course } from './types/interface';
+
+const logo = require('./logo.svg');
+
+>>>>>>> initial attempt, not working
 interface State {
 }
 
