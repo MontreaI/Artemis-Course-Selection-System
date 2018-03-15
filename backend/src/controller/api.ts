46667a5ee31626bca9db1181f404293af2bee18f
Raymond Chan <rca71@sfu.ca>
2018-03-14 21:19:04 -0700

    Completed initial code set-up of API calls to SFU course website

--- backend/src/controller/api.ts
+++ backend/src/controller/api.ts
@@ -1,6 +1,7 @@
 'use strict';
 import { Response, Request } from 'express';
 import http from 'http';
+import { isUndefined } from 'util';
 
 export let getApi = (req: Request, res: Response) => {
     const courses = [{ name: 'CMPT470' }];
@@ -9,8 +10,106 @@ export let getApi = (req: Request, res: Response) => {
     res.end();
 };
 
+/*
+GET /bin/wcm/course-outlines
+REST wrapper for the Course Outlines data. Returns a list of years.
+*/
 export let getYears = (req: Request, res: Response) => {
     http.get('http://www.sfu.ca/bin/wcm/course-outlines', (response) => {
+        if (response.statusCode != 200) {
+            throw new Error('Could not fetch from server');
+        }
+        else {
+            let jsonData = '';
+            response.on('data', (chunk) => { jsonData += chunk; });
+            response.on('end', () => {
+                try {
+                    res.write(jsonData);
+                    res.end();
+                } catch (e) {
+                    console.error(e.message);
+                }
+            });
+        }
+    });
+};
+
+/*
+The year in the format yyyy (e.g. 2015). Alternately, the dynamic variable 'current' and 'registration can be used.
+'current' - will use the year of the current active term
+'registration' - will use the year of the current registration term.
+*/
+export let getTerms = (req: Request, res: Response, year: string, registration?: string) => {
+    if (registration != undefined) {
+        registration = '';
+    }
+    else {
+        registration = '/' + registration;
+    }
+    http.get('http://www.sfu.ca/bin/wcm/course-outlines?' + year + registration, (response) => {
+        if (response.statusCode != 200) {
+            throw new Error('Could not fetch from server');
+        }
+        else {
+            let jsonData = '';
+            response.on('data', (chunk) => { jsonData += chunk; });
+            response.on('end', () => {
+                try {
+                    res.write(jsonData);
+                    res.end();
+                } catch (e) {
+                    console.error(e.message);
+                }
+            });
+        }
+    });
+};
+
+// Returns a list of course numbers that includes the course title for the given department.
+export let getCourseNumbers = (req: Request, res: Response, year: string, term: string, department: string) => {
+    http.get('http://www.sfu.ca/bin/wcm/course-outlines?' + year + '/' + term + '/' + department, (response) => {
+        if (response.statusCode != 200) {
+            throw new Error('Could not fetch from server');
+        }
+        else {
+            let jsonData = '';
+            response.on('data', (chunk) => { jsonData += chunk; });
+            response.on('end', () => {
+                try {
+                    res.write(jsonData);
+                    res.end();
+                } catch (e) {
+                    console.error(e.message);
+                }
+            });
+        }
+    });
+};
+
+// Returns a list of course sections for the given course number.
+export let getCourseSections = (req: Request, res: Response, year: string, term: string, department: string, courseNumber: string) => {
+    http.get('http://www.sfu.ca/bin/wcm/course-outlines?' + year + '/' + term + '/' + department + '/' + courseNumber, (response) => {
+        if (response.statusCode != 200) {
+            throw new Error('Could not fetch from server');
+        }
+        else {
+            let jsonData = '';
+            response.on('data', (chunk) => { jsonData += chunk; });
+            response.on('end', () => {
+                try {
+                    res.write(jsonData);
+                    res.end();
+                } catch (e) {
+                    console.error(e.message);
+                }
+            });
+        }
+    });
+};
+
+// Returns the content details of a specific course outline.
+export let getCourseOutline = (req: Request, res: Response, year: string, term: string, department: string, courseNumber: string, courseSection: string) => {
+    http.get('http://www.sfu.ca/bin/wcm/course-outlines?' + year + '/' + term + '/' + department + '/' + courseNumber + '/' + courseSection, (response) => {
         if (response.statusCode != 200) {
             throw new Error('Could not fetch from server');
         }
