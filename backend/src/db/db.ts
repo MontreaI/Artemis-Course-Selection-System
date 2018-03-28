78c9896821b72126ebc5d99d360f76c4ef81c779
Thai Quoc Nguyen <thaiquocnguyen@outlook.com>
2018-03-27 20:55:05 -0700

    DB complete

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,3 +1,5 @@
+import { lchmod } from 'fs';
+
 // import { IMain, IDatabase } from 'pg-promise';
 // import * as pgPromise from 'pg-promise';
 
@@ -7,48 +9,140 @@ const connectionString: string = 'postgres://ykprolcv:8KT4k4_Gzm2JGTwbWdx-5RbfmN
 const pgp = require('pg-promise')();
 const db = pgp(connectionString);
 
-interface User {
+export interface User {
     username: string;
+    password: string;
+    email: string;
+}
+
+export interface Course {
+    id: number;
+    department: string;
+    number: number;
+    section: string;
+    year: number;
+    term: string;
+    description: string;
 }
 
 export function createTableUsers() {
-    db.none('CREATE TABLE IF NOT EXISTS users ' +
+    return db.none('CREATE TABLE IF NOT EXISTS users ' +
         '(' +
-        'username text NOT NULL PRIMARY KEY' +
-        'password text NOT NULL' +
+        'username text NOT NULL PRIMARY KEY,' +
+        'password text NOT NULL,' +
+        'email text' +
         ')')
-      .catch((error: Error) => {
+    .then(() => {
+        return true;
+    })
+    .catch((error: Error) => {
         global.console.log(error);
-      });
+        return false;
+    });
 }
 
 export function createTableCourses() {
-    db.none('CREATE TABLE IF NOT EXIST courses ' +
+    return db.none('CREATE TABLE IF NOT EXISTS courses ' +
             '(' +
-            'id integer PRIMARY KEY,' +
+            'id serial PRIMARY KEY,' +
             'department varchar(4) NOT NULL,' +
-            'number integer NOT NULL' +
-            'section integer NOT NULL' +
+            'number integer NOT NULL,' +
+            'section text NOT NULL,' +
+            'year integer NOT NULL,' +
+            'term text NOT NULL,' +
+            'description text' +
             ')')
-      .catch((error: Error) => {
+    .then(() => {
+        return true;
+    })
+    .catch((error: Error) => {
         global.console.log(error);
-      });
+        return false;
+    });
 }
 
-export function findUser(username: string, password: string): Promise<boolean> {
-    return db.one('SELECT * FROM users WHERE username = $1 AND password = $2', username, password)
-             .then((data: User) =>  true)
-             .catch((err: Error) => {
-                 global.console.log(err);
-                 return false;
-             });
+export function createTableUserCourse() {
+    return db.none('CREATE TABLE IF NOT EXISTS UserCourse ' +
+        '(' +
+        'username text NOT NULL,' +
+        'id integer NOT NULL,' +
+        'PRIMARY KEY (username, id)' +
+        ')')
+    .then(() => {
+        return true;
+    })
+    .catch((error: Error) => {
+        global.console.log(error);
+        return false;
+    });
 }
 
-export function createUser(username: string, password: string): Promise<boolean> {
-    return db.none('INSERT INTO users(username, password) VALUES($1, $2)', username, password)
+/*
+ * Accepts an user object. The object can have username and password for login
+ * or just username for admin lookup
+*/
+export function findUser(user: User) {
+    if (user.password === '') {
+        return db.one('SELECT * FROM users WHERE username = $1', user.username)
+            .then((data: User) => data)
+            .catch((err: Error) => {
+                global.console.log(err);
+                return { username: '', password: '', email: '' };
+            });
+    } else {
+        return db.one('SELECT * FROM users WHERE username = $1 AND password = $2', [user.username, user.password])
+                 .then((data: User) => data)
+                 .catch((err: Error) => {
+                     global.console.log(err);
+                     return { username: '', password: '', email: '' };
+                 });
+    }
+}
+
+export function createUser(user: User) {
+    return db.none('INSERT INTO users(username, password, email) VALUES($1, $2, $3)', [user.username, user.password, user.email])
              .then(() => true)
              .catch((error: Error) => {
                  global.console.log(error);
                  return false;
              });
+}
+
+export function createCourse(course: Course) {
+    return db.none('SELECT * FROM courses WHERE department = $1 AND number = $2 AND section = $3 AND year = $4 AND term = $5',
+                  [ course.department,
+                    course.number,
+                    course.section,
+                    course.year,
+                    course.term ])
+            .then(() => {
+                return db.one('INSERT INTO courses(department, number, section, year, term, description) VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+                              [ course.department,
+                                course.number,
+                                course.section,
+                                course.year,
+                                course.term,
+                                course.description ])
+                       .then((data: Course) => {
+                           return data.id;
+                       })
+                       .catch((err: Error) => {
+                           global.console.log(err);
+                           return -1;
+                       });
+            })
+            .catch((err: Error) => {
+                return -1;
+            });
+}
+
+export function addUserCourse(username: string, courseID: number) {
+    return db.none('INSERT INTO UserCourse(username, id) VALUES ($1, $2)', [username, courseID])
+        .then(() => {
+            return true;
+        })
+        .catch((err: Error) => {
+            global.console.log(err);
+            return false;
+        });
 }
