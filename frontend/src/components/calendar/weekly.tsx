71ea76664df90014ce39ae7a2e2edf0490961155
Thai Nguyen <thai_ng@outlook.com>
2018-03-28 22:35:06 -0700

    some styling

--- frontend/src/components/calendar/weekly.tsx
+++ frontend/src/components/calendar/weekly.tsx
@@ -8,7 +8,7 @@ import './weekly.css';
 class WeeklyView extends React.Component {
     render() {
         let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-        let daysCols = days.map((day: string) => <th key={day} >{day}</th>);
+        let daysCols = days.map((day: string) => <th className={'timeCell'} key={day} >{day}</th>);
 
         var times = Array.apply(0, Array((22 - 8) * 2)).map((_: number, index: number) => {
             return index % 2 === 0 ?  `${(index / 2) + 8}:00` : `${((index - 1) / 2) + 8}:30`;
