b38d0e17b8300032d458835527edabe82911a2c6
Thai Nguyen <thai_ng@outlook.com>
2018-04-06 14:55:00 -0700

    Display course conflict

--- frontend/src/components/calendar/weekly.tsx
+++ frontend/src/components/calendar/weekly.tsx
@@ -72,7 +72,24 @@ class WeeklyView extends React.Component<{}, State> {
                             }
                         }
                     ]
-                }
+                },
+                {
+                    name: 'CMPT469',
+                    time: [
+                        {
+                            day: Day.Thursday,
+                            start: {
+                                hour: 15,
+                                half: false
+                            },
+                            end: {
+                                hour: 18,
+                                half: false
+                            }
+                        }
+                    ]
+                },
+
             ]
         };
     }
@@ -85,31 +102,53 @@ class WeeklyView extends React.Component<{}, State> {
         for (let t = 0; t < (22 - 8) * 2; t++) {
             let elems = [];
             for (let d = 0; d < days.length; d++) {
-                let hasCourse = false;
+                let courses: string[] = [];
                 for (let course of this.state.courses) {
                     for (let time of course.time) {
                         if (time.day === d) {
-                            hasCourse = true;
                             let hour = 0;
-                            let timeString = '';
+                            let halfHour = false;
 
                             if (t % 2 === 0) {
                                 hour = (t / 2) + 8;
-                                timeString = `${hour}:00`;
-                            } else {
+                            }
+                            else {
+                                halfHour = true;
                                 hour = ((t - 1) / 2) + 8;
-                                timeString = `${hour}:30`;
                             }
 
-                            if (hour >= time.start.hour && hour <= time.end.hour) {
-                                elems.push(<td className={'timeCell classTime'} key={`${course.name}${time.day}`}>{course.name}</td>);
-                            } else {
-                                elems.push(<td className={'timeCell'} key={`${d}${t}`}>{timeString}</td>);
+                            if (hour === time.start.hour) {
+                                if (halfHour) {
+                                    courses.push(course.name);
+                                } else {
+                                    if (!time.start.half) {
+                                        courses.push(course.name);
+                                    }
+                                }
+                            }
+                            else if (hour === time.end.hour) {
+                                if (!halfHour) {
+                                    if (time.end.half) {
+                                        courses.push(course.name);
+                                    }
+                                }
+                            }
+                            else if (hour > time.start.hour &&
+                                     hour < time.end.hour) {
+                                     courses.push(course.name);
                             }
                         }
                     }
                 }
-                if (!hasCourse) {
+
+                if (courses.length > 0) {
+                    if (courses.length > 1) {
+                        let coursesStr = courses.join(' ');
+                        elems.push(<td className={'timeCell timeDup'} key={`${coursesStr}${d}`}>{coursesStr}</td>);
+                    } else {
+                        elems.push(<td className={'timeCell classTime'} key={`${courses[0]}${d}`}>{courses[0]}</td>);
+                    }
+                } else {
                     let timeString = t % 2 === 0 ? `${(t / 2) + 8}:00` : `${((t - 1) / 2) + 8}:30`;
                     elems.push(<td className={'timeCell'} key={`${d}${t}`}>{timeString}</td>);
                 }
